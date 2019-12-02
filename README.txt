79ad132cce2f23aaccc1e273df2602f63ff58bd8
Bruno Bucciotti <bruno.bucciotti@sns.it>
2019-12-02 18:51:38 +0100

    Fixati i doubles in giro. Ora stampiamo un modo normale senza non linearità. Il conto teorico della sua frequenza torna bene.

--- README.txt
+++ README.txt
@@ -3,3 +3,17 @@ Questi file contengono ciascuno lo stato dei displacement (tutti e 32) nel tempo
 Lo script di gnuplot (che si lancia con gnuplot gnuscript) stampa i grafici in .png.
 Eseguire poi convert -delay 25 *.png ANI.gif per stampare ANI.gif, che mette tutto in fila.
 Nota bene: i file .png sono stati paddati con zeri davanti fino a lunghezza 2 (%02i in gnuscript). Poiché convert va in ordine lessicografico, se si passano le 99 png assicurarsi di paddare con più zeri.
+
+Ora viene stampato un modo normale (il secondo più lungo). E'possibile calcolare il periodo di tale modo come si vede in gif.
+Avendo messo omega=1 ci si può calcolare la frequenza di questo modo
+(trovare autovalori di matrice
+2 -1  0  0 .. -1
+-1 2 -1  0 ..  0
+0 -1  2 -1 ..  0
+..
+-1 ..     0 -1 2
+con mathematica e questo viene 0.152241)
+
+Nel tempo "macchina" 50dt=0.5 scatto una foto, che fisicamente (avendo usato -delay 25) vuol dire 0.25s.
+Dunque T_macchina = 2pi/omega e T_gif = (T_macchina/0.5) * 0.25 secondi. Viene T_gif_teorico = 8.05s, mentre prendendo 11 misure
+con la gif si ottiene in media T_gif_sperimentale = 8.07s.
--- esempio.f90
+++ esempio.f90
@@ -5,21 +5,31 @@ implicit none
 ! C'è anche un accoppiamento alpha/3 (u_{i+1} - u_i)^3 fra contigui
 
 integer, parameter :: N=32 ! # oscillatori
-real*8, parameter :: alpha = 0.25 ! costante nonlineare
+real*8, parameter :: alpha = 0.25d0 ! costante nonlineare
+real*8, parameter :: PI = 4.d0*Datan(1.d0) ! costante nonlineare
 !integer, parameter :: nmis = 1000 ! numero di misure
 !integer, parameter :: DT = 20 ! tempo fra una misura e l'altra
 integer, parameter :: iterazioni = 4500 ! temporaneamente, numero di iterazioni
-real*8, parameter :: dt = 0.01 ! intervallo di tempo fra uno step e il seguente
+real*8, parameter :: dt = 0.01d0 ! intervallo di tempo fra uno step e il seguente
 real*8 chain(N) ! catena di particelle, registro i displacement
 real*8 chainD(N) ! catena di particelle, registro le velocità
 real*8 support(3) ! serve per registrare i displacement per calcolare lo step successivo
 integer iter,i
+real*8 k
 
+
+! breve cenno teorico
+! se ho N palline su un cerchio attaccate con molle
+! gli automodi sono u_j = A sin(k_{n,N} * j - omega * t)
+! dove k_{n,N} = 2pi n/N
+
+k = 2.d0*PI/N
 
 ! setup del lattice
-chain = 0.
-chainD = 0.
-chain(16) = 1.0
+do i=1,N
+	chain(i) = sin(2.d0*k*i)
+enddo
+chainD = 0.d0
 
 !per ogni iterazione
 do iter=1, iterazioni
@@ -31,7 +41,7 @@ do iter=1, iterazioni
 		support(2) = chain(i)
 		support(3) = chain(i+1)
 		!ricalcola p
-		chainD(i) = chainD(i) - dt * (2*support(2)-support(1)-support(3))
+		chainD(i) = chainD(i) - dt * (2.d0*support(2)-support(1)-support(3))
 		!salva la posizione del centrale attuale come quello a sinistra del passo dopo
 		support(1) = chain(i)
 		!ricalcola x usando la p già aggiornata
@@ -39,12 +49,15 @@ do iter=1, iterazioni
 	enddo
 	! ultima particella a mano, per non fare casino con chain(N+1) che non esiste
 	!ricalcola p
-	chainD(N) = chainD(N) - dt * (2*chain(N)-chain(N-1)-chain(1))
+	chainD(N) = chainD(N) - dt * (2.d0*chain(N)-chain(N-1)-chain(1))
 	!ricalcola x usando la p già aggiornata
 	chain(N) = chain(N) + dt * chainD(N)
 	! fine aggiornamento reticolo
 	if (modulo(iter, 50) == 0) then
-		write(*,*) "Test", iter
+		! visivamente con questa riga si stampa due volte lo stesso punto ai due estremi.
+		! studiando i modi normali senza questa, l'estremo destro era un nodo, quello sinistro era solo vicino a un nodo
+		write(iter/50 + 6,*) chain(N)
+		!stampa normalmente. il +6 viene dal tumore di fortran a cui non piace write(5/6,*)
 		do i=1, N
 			write(iter/50 + 6,*) chain(i)
 		enddo
